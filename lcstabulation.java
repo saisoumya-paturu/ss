d0a0d072f5e4d990f8310da7702a6d9cb6bf2a0a
saisoumya-paturu <saisoumyapaturu@gmail.com>
2025-06-13 15:19:33 +0530

    Create lcstabulation.java

---
+++ lcstabulation.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static int lcsdp(String x, String y) {
+        int m = x.length(), n = y.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+
+        int result = lcsdp(s1, s2);
+
+        System.out.println("Length of LCS is " + result);
+    }
+}
